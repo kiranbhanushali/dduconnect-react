fe2e5475159edf571311cba8da4c0cdf81676d81
Kiran Bhanushali <kiranbhanushali143.kb143@gmail.com>
2021-03-17 22:48:51 +0530

    Refactor 0

--- android/app/src/main/java/com/dduconnect/dduconnect/MainActivity.java
+++ android/app/src/main/java/com/dduconnect/dduconnect/MainActivity.java
@@ -1,4 +1,4 @@
-package com.ddu_connect;
+package com.dduconnect.dduconnect;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "DDU_CONNECT";
+    return "DDUCONNECT";
   }
 }
--- ios/DDUCONNECTTests/DDUCONNECTTests.m
+++ ios/DDUCONNECTTests/DDUCONNECTTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DDU_CONNECTTests : XCTestCase
+@interface DDUCONNECTTests : XCTestCase
 
 @end
 
-@implementation DDU_CONNECTTests
+@implementation DDUCONNECTTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
